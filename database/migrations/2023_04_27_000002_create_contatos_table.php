b48dd3dd66584ba2e8b36c841c4a1b25ad37a956
EDUARDO GOMES DE SA <99502227+EduardoGomesSa@users.noreply.github.com>
2023-05-01 21:17:04 -0300

    criação do resource de contato

--- database/migrations/2023_04_27_000002_create_contatos_table.php
+++ database/migrations/2023_04_27_000002_create_contatos_table.php
@@ -13,9 +13,9 @@ return new class extends Migration
     {
         Schema::create('contatos', function (Blueprint $table) {
             $table->id();
-            $table->string('celular')->unique();
-            $table->string('telefone')->unique();
-            $table->string('email')->unique();
+            $table->string('celular');
+            $table->string('telefone');
+            $table->string('email');
             $table->timestamps();
         });
     }
