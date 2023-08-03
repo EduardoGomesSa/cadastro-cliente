9491640c4d82b5a98a813292be162ba7a1e607ff
EDUARDO GOMES DE SA <99502227+EduardoGomesSa@users.noreply.github.com>
2023-08-02 23:49:38 -0300

    remoção da interface front end, deixando apenas a api

--- routes/api.php
+++ routes/api.php
@@ -1,14 +1,19 @@
 <?php
 
-use App\Http\Controllers\ClienteController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
-
-Route::post('/cliente', [ClienteController::class, 'store']);
-Route::get('/cliente', [ClienteController::class, 'index']);
-Route::put('/cliente', [ClienteController::class, 'update']);
-Route::delete('/cliente', [ClienteController::class, 'destroy']);
