1c98c6da17ef26a9d5a68a43802612e59822d3a1
EDUARDO GOMES DE SA <99502227+EduardoGomesSa@users.noreply.github.com>
2023-08-10 23:40:49 -0300

    criação do método de atualizar contato

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -2,11 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\ContatoRequest;
+use App\Models\Contato;
 use Illuminate\Http\Request;
 
 class ContatoController extends Controller
 {
-    public function update(){
+    private $contato;
 
+    public function __construct(Contato $contato)
+    {
+        $this->contato = $contato;
+    }
+
+    public function update(ContatoRequest $request, $id){
+        $contatoExiste = $this->contato->find($id);
+
+        if($contatoExiste){
+            $contatoExiste->update($request->all());
+
+            return response(["message"=>"contato atualizado com sucesso"])->setStatusCode(200);
+        }
+
+        return response(["rrror"=>"contato não existe"])->setStatusCode(401);
     }
 }
